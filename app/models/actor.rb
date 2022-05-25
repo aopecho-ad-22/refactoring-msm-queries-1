e91c759be54b329728821d8c7b4c57c2e9652600
aopecho <aopecho@uchicago.edu>
2022-05-25 18:26:49 +0000

    I am a dumb dumb who wrote some very messy code

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,17 +11,16 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
-  def filmography
-    starred_in = self.director_id
-    matched_director_id = Director.where({ :id => directed_by_id })
-    the_director = matched_director_id.at(0)
-    return the_director
-  end
 
   def characters
     actor_id = self.id
     roles_played = Character.where({ :actor_id => actor_id })
     return roles_played
   end
-end
 
+  def movie
+    movie_starred_in = self.movie_id.at(0)
+    return movie_starred_in
+  end
+  
+end
